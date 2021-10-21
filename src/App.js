b64812d733b931ec3262b5881566b04d90d76105
yandali <ydamianelguera@gmail.com>
2021-10-21 10:15:02 -0500

    Form de login en proceso

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import Nav from './views/Nav.js'
 import LogIn from './views/LogIn.js'
+import LogIn1 from './views/LogIn1.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <Nav /> */}
-        <LogIn />
+        {/* <LogIn /> */}
+        <LogIn/>
       </header>
     </div>
   );

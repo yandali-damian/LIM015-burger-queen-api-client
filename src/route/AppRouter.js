3641b54d3053779b1b268c2a68889d365652ff6c
yandali <ydamianelguera@gmail.com>
2021-10-22 01:09:45 -0500

    proceso de implementacion de login/ alerta

---
+++ src/route/AppRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import LogIn from '../views/LogIn.js';
+import Home from '../views/Home.js';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+  export default function AppRouter() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/">
+              <LogIn />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+ 

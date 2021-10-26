e2fc758d59fab41e192dd80e4a56a1723b7baf09
yandali <ydamianelguera@gmail.com>
2021-10-25 21:51:50 -0500

    se modulariza los estilos de la vista home, falta mejorar los botones de los menus

---
+++ src/style/styleHome.js
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/core/styles";
+import logo1 from '../img/logo1.png';
+import fondoHome from '../img/contenedor.png';
+
+export const useStyles = makeStyles(theme => ({
+ 
+    fondoHome: {
+        backgroundImage: `url(${fondoHome})`,
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+        backgroundSize: 'cover',
+        fontFamily: 'Averia Libre',
+    },
+    logo:{
+      backgroundImage:`url(${logo1})`,
+      width: '6%',
+      height: '6%',
+    },
+    ToolbarColor:{
+      backgroundColor: '#000',
+      borderBottom: '3px  solid #fb970e',
+    }
+  }));

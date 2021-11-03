bf22c8b7b48632677cb0664a47962c1e30a5ad72
paolataboada <taboadapaola3@gmail.com>
2021-11-02 23:48:27 -0500

    subí cambios para merge

--- src/views/Order.js
+++ src/views/Order.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Grid, Stack } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import { useStyle } from '../style/styleOrder.js';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import send from '../img/sendButton.png';
@@ -11,21 +11,51 @@ import { ThemeProvider } from "@mui/material/styles";
 // import { theme } from '../style/styleLogin.js';
 
 const themeHome = createTheme({
-  palette: {
-    primary: {
-      main: "#9CDD2B"
+    palette: {
+        primary: {
+            main: "#9CDD2B"
+        },
+        tamaño: {
+            with: '10',
+            height: '10',
+            border: '50%'
+        }
     },
-    tamaño:{
-      with:'10',
-      height:'10',
-      border:'50%'
+    typography: {
+        fontSize: 15,
+        fontFamily: ["Arial"]
     }
-  },
-  typography: {
-    fontSize: 15,
-    fontFamily: ["Arial"]
-  }
 });
+
+const handleSubmit = async () => {
+
+    const response = await fetch('https://burguer-queen-api-lim015.herokuapp.com/orders', {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+
+    const data = await response.json();
+    return data.filter((item) => {
+        return item.products.map((el) => {
+            const currentOrder = {
+                'client': item.client,
+                'order': el.productId.name,
+                'price': el.productId.price
+            }
+            console.log({
+                'client': item.client,
+                'order': el.productId.name,
+                'price': el.productId.price
+            })
+            return currentOrder
+        })
+        // console.log(39, item.products)
+        // return item._id;
+        // return producto.type === tipoProducto;
+    });
+    // console.log(42, result) // Array de objetos (lo mismo que línea 37)
+}
+console.log(44, handleSubmit()) // Promesa pendiente
 
 export default function Pedidos() {
     const classe = useStyle();
@@ -43,64 +73,37 @@ export default function Pedidos() {
         })
         return enterOrder + ' s/. 2.50'
     }
-    console.log(17, selectOrder(pedidos))
+    // console.log(17, selectOrder(pedidos))
 
     return (
         <>
             <Grid container direction="row" className={classe.containerOrderList}>
                 <Grid item xs={4} justifyContent="center">
-                    <input placeholder='Nombre' style={{ color: '#000', background: '#fff', borderRadius: '20px', padding: '12px'}}/>
+                    <input placeholder='Nombre' style={{ color: '#000', background: '#fff', borderRadius: '20px', padding: '12px' }} />
                 </Grid>
                 <Grid item xs={8} >
                     <h1 style={{ color: '#fff', textAlign: 'center' }}>Lista de Pedidos</h1>
                 </Grid>
             </Grid>
+
             <Grid container direction="row" className={classe.containerOrderList}>
-            <ThemeProvider theme={themeHome}>
+                <ThemeProvider theme={themeHome}>
                     <Box sx={{ "& > :not(style)": { m: 0.2 } }}>
                         <Fab size="small" color="primary" aria-label="añadir" background="#FB970E" onClick={() => {
-                                setCount(count + 1);
-                            }}>
-                        +
+                            setCount(count + 1);
+                        }}>
+                            +
                         </Fab>
                         <Fab size="small" color="#fff" variant="contained">
-                        1
+                            {count}
                         </Fab>
                         <Fab size="small" color="primary" aria-label="quitar" onClick={() => {
-                                setCount(Math.max(count - 1, 0));
-                            }}>
-                        -
+                            setCount(Math.max(count - 1, 0));
+                        }}>
+                            -
                         </Fab>
                     </Box>
                 </ThemeProvider>
-                {/* <Grid item xs> */}
-                    {/* <Stack direction="row">
-                        <Button
-                            sx={{ borderRadius: '50px', background: '#fff', color: '#000', padding: '0', fontSize: '20px' }}
-                            aria-label="añadir"
-                            onClick={() => {
-                                setCount(count + 1);
-                            }}
-                        >
-                            +
-                        </Button>
-                        <Button
-                            variant="contained"
-                            sx={{ borderRadius: '50px', color: '#fff', padding: '0', fontSize: '20px' }}
-                        >
-                            {count}
-                        </Button>
-                        <Button
-                            sx={{ borderRadius: '40%', background: '#fff', color: '#000', padding: '0'}}
-                            aria-label="quitar"
-                            onClick={() => {
-                                setCount(Math.max(count - 1, 0));
-                            }}
-                        >
-                            -
-                        </Button>
-                    </Stack> */}
-                {/* </Grid> */}
                 <Grid item xs={7}>
                     <span className={classe.orderMenu}>{selectOrder(pedidos)}</span>
                 </Grid>
@@ -108,6 +111,7 @@ export default function Pedidos() {
                     <DeleteForeverIcon className={classe.btnDelete} />
                 </Grid>
             </Grid>
+
             <Grid container direction="row" className={classe.containerOrderList} sx={{ justifyContent: 'space-around' }}>
                 <Button
                     variant="outlined"

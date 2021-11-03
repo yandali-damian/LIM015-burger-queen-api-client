fe23b6bc575872fcfde62690eab4a3c6766ef4ba
yandali <ydamianelguera@gmail.com>
2021-11-02 23:44:49 -0500

    se reorganiza la vista home

--- src/views/Home.js
+++ src/views/Home.js
@@ -10,11 +10,13 @@ import { useStyles,themeHome} from '../style/styleHome.js';
 import { useStyle } from '../style/styleOrder.js';
 import logo1 from '../img/logo1.png';
 import { getProducts } from '../services/Product.js';
-import Pedidos from '../views/Order.js'
 import ArrowBackIosTwoToneIcon from '@mui/icons-material/ArrowBackIosTwoTone';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '../style/styleLogin.js';
-
+import send from '../img/sendButton.png';
+import Fab from "@mui/material/Fab";
+import { createTheme } from "@mui/material/styles";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 export default function Home() {
 
@@ -47,6 +49,29 @@ export default function Home() {
       setMenu('');
       setBebidas(producto);
     });
+  }
+  const themepedido = createTheme({
+    palette: {
+      primary: {
+        main: "#9CDD2B"
+      },
+      tamaño:{
+        with:'10',
+        height:'10',
+        border:'50%'
+      }
+    },
+    typography: {
+      fontSize: 15,
+      fontFamily: ["Arial"]
+    }
+  });
+  /*funcion para agregar un pedido */
+
+  
+
+  const AgregarPedido = producto => () => {
+    console.log(producto);
   }
 
   const classes = useStyles();
@@ -103,7 +128,7 @@ export default function Home() {
                   <li key={desayuno._id}>
                     <Grid container direction='row'>
                       <Grid item xs={7}>
-                        <Button color="secondary">{desayuno.name}</Button>
+                        <Button color="secondary" onClick={AgregarPedido(desayuno)}>{desayuno.name}</Button>
                       </Grid>
                       <Grid item xs={4}>
                         <Button color="primary" >S/.{desayuno.price}</Button>
@@ -148,12 +173,51 @@ export default function Home() {
           </Grid>
         </ThemeProvider>
           <Grid item xs={7} component='main' sx={{ backgroundColor: 'rgba(1 1 1 / 34%)'}}>
-            <Pedidos className={classe.containerOrderList} />
+          <Grid container direction="row" className={classe.containerOrderList}>
+                <Grid item xs={4} justifyContent="center">
+                    <input placeholder='Nombre del cliente' style={{ color: '#000', background: '#fff', borderRadius: '20px', padding: '12px'}}/>
+                </Grid>
+                <Grid item xs={8} >
+                    <h1 style={{ color: '#fff', textAlign: 'center' }}>Lista de Pedidos</h1>
+                </Grid>
+            </Grid>
+
+            {/* <Pedidos className={classe.containerOrderList} /> */}
+
+            <Grid container direction="row" className={classe.containerOrderList}>
+                <Grid item xs={4}>
+                    <ThemeProvider theme={themepedido}>
+                        <Box sx={{ "& > :not(style)": { m: 0.2 } }}>
+                            <Fab size="small" color="primary" aria-label="quitar">
+                            -
+                            </Fab>
+                            <input style={{ color: '#000', background: '#fff', borderRadius: '7px', padding: '12px', width:'20px'}}/>
+                            <Fab size="small" color="primary" aria-label="añadir" background="#FB970E">
+                            +
+                            </Fab>
+                        </Box>
+                    </ThemeProvider>
+                </Grid>
+                <Grid item xs={6}>
+                    <span>sdczsdvcS</span>
+                </Grid>
+                <Grid item xs={2}>
+                    <DeleteForeverIcon className={classe.btnDelete} />
+                </Grid>
+              </Grid>
+            <Grid container direction="row" className={classe.containerOrderList} sx={{ justifyContent: 'space-around' }}>
+                <Button
+                    variant="outlined"
+                    sx={{ borderRadius: '30px', color: '#fff', px: '50px', py: '15px', fontSize: '25px', background: '#000', border: '5px solid #9CDD2B' }}
+                >
+                    Pedir
+                    <img style={{ paddingLeft: '10px', height: '35px' }} src={send} alt="Botón enviar" />
+                </Button>
+            </Grid>
           </Grid>
         </Grid>
       </Box>
     </Box>
-    // </ThemeProvider> 
   );
 }
 
